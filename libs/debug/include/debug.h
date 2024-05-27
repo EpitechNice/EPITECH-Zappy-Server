61e2835cce8c6287ed490414928d8a2c3465b1dc
H4rdeol <leonard.oursel@epitech.eu>
2024-05-27 19:02:29 +0200

    [~] WIP

---
+++ libs/debug/include/debug.h
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2024
+** library
+** File description:
+** linked_list
+*/
+
+#ifndef INCLUDED_DEBUG_H
+    #define INCLUDED_DEBUG_H
+
+    /*  ---- INCLUDE ----*/
+
+    #include <stdbool.h>
+
+    /*  ---- STRUCTURE ----*/
+
+typedef enum {
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARNING,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_CRITICAL,
+} log_level_t;
+
+    /*  ---- FUNCTION ----*/
+
+#endif /* LINKED_LIST_H_ */
--- src/utils/digit.c
+++ src/utils/digit.c
@@ -9,9 +9,7 @@
 
 bool is_digit(char c)
 {
-    if (c >= '0' && c <= '9')
-        return true;
-    return false;
+    return (c >= '0' && c <= '9');
 }
 
 bool is_digits(char *str)
