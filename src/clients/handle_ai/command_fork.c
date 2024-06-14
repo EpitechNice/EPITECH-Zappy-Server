d34752e49f4cf7411e7fa198c4acd9b7443bb9b4
Dan13615 <daniil.stepanov@epitech.eu>
2024-06-14 21:06:08 +0200

    [~] fix fork

--- src/clients/handle_ai/command_fork.c
+++ src/clients/handle_ai/command_fork.c
@@ -10,9 +10,17 @@
 void command_fork(UNUSED char **args, client_t *client)
 {
     game_t *game = get_server()->game;
+    team_t *team;
 
     LOG(LOG_LEVEL_INFO, "Client of team %s forked itself (at %i %i)",
         client->team_name, client->x, client->y);
+    for (lnode_t *node = game->teams; node; node = node->next) {
+        team = node->data;
+        if (strcmp(team->name, client->team_name) == 0) {
+            team->clients_nb++;
+            break;
+        }
+    }
     dl_push_back(&game->map[client->y][client->x].eggs, strdup("EGG HAS BEEN PLANTED"));
     dl_push_back(&client->to_send, strdup("ok"));
 }
