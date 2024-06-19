89c632924cf2d56ba3e304fa16231af6b773cf9d
Dan13615 <daniil.stepanov@epitech.eu>
2024-06-19 23:55:30 +0200

    [+] ppo in ai left

--- src/clients/handle_ai/command_left.c
+++ src/clients/handle_ai/command_left.c
@@ -6,15 +6,22 @@
 */
 
 #include "ai.h"
+#include "gui.h"
 
 void command_left(UNUSED char **args, client_t *client)
 {
-    if (client->direction == UP) {
-        client->direction = LEFT;
-        return;
-    }
+    char *fd = NULL;
+    UNUSED int _ = asprintf(&fd, "%d", client->fd);
+
     client->direction--;
+    if (client->direction < 0)
+        client->direction = LEFT;
     LOG(LOG_LEVEL_DEBUG, "Client of team %s turned left", client->team_name);
     client->next_action_time = get_server()->global_time_stamp + 7;
     dl_push_back(&client->to_send, strdup("ok"));
+    for (lnode_t *tmp = get_server()->clients; tmp != NULL; tmp = tmp->next)
+        if (((client_t *)(tmp->data))->status == GUI)
+            command_ppo((char *[]){"ppo", fd, NULL},
+                (client_t *)(tmp->data));
+    free(fd);
 }
