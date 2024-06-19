8b910a6562f828277b802a0587d079d7955baba3
Dan13615 <daniil.stepanov@epitech.eu>
2024-06-19 23:43:45 +0200

    [+] ppo when ai right

--- src/clients/handle_ai/command_right.c
+++ src/clients/handle_ai/command_right.c
@@ -6,12 +6,21 @@
 */
 
 #include "ai.h"
+#include "gui.h"
 
 void command_right(UNUSED char **args, client_t *client)
 {
+    char *fd = NULL;
+    UNUSED int _ = asprintf(&fd, "%d", client->fd);
+
     client->direction++;
     client->direction %= LEFT + 1;
     LOG(LOG_LEVEL_DEBUG, "Client of team %s turned right", client->team_name);
     client->next_action_time = get_server()->global_time_stamp + 7;
     dl_push_back(&client->to_send, strdup("ok"));
+    for (lnode_t *tmp = get_server()->clients; tmp != NULL; tmp = tmp->next)
+        if (((client_t *)(tmp->data))->status == GUI)
+            command_ppo((char *[]){"ppo", fd, NULL},
+                (client_t *)(tmp->data));
+    free(fd);
 }
